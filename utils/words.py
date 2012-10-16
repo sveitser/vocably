f1c20ac34510b68a30345f0d81d5095c229095d7
Mathis Antony <sveitser@gmail.com>
2012-10-16 09:51:47 +0800

    load wordlist

--- utils/words.py
+++ utils/words.py
@@ -18,30 +18,28 @@ def filters():
 def is_english(word):
     return any([f(word) for f in filters()]) and word.isalpha()
 
-def filter_dictionary(function, dictionary):
-    for w in dictionary:
-        if not function(w):
-            dictionary.pop(w)
+def create_wordlist_csv(fname):
+    """
+    Loads a list from file. Returns normalized {word:freq} dictionary.
 
-    return dictionary
+    Column1: words
+    Column2: some frequency measure
+    """
+    f = open(fname, 'r')
+    d = {}
+    total = 0.0
 
+    for line in f.readlines():
+        word, freq = line.split()
+        if is_english(word):
+            d[word] = float(freq)
+            total += float(freq)
+    
+    f.close()
 
+    for word in d:
+        d[word] /= total
 
-#f = open('data/en.txt','r')
-#of = open('data/en_pyenchant_or_nltk_only.txt','w')
-#en_dict = enchant.Dict("en_GB")
-#rank = 1
-#for line in f.readlines():
-#    data = line.split()
-#    try:
-#        word = data[0]
-#        if word.isalpha() and (en_dict.check(word) or wordnet.synsets(word)):
-#            of.write(' '.join([str(rank), data[1], data[0]]) + '\n')
-#            rank += 1
-#    except:
-#        pass
-#
-#
-#print '%i words written' % rank 
-#of.close()
-#f.close()
+    return d
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
