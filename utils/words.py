4b0e3dde7a3d465dec442c5b80e54a74bfc1932c
Mathis Antony <sveitser@gmail.com>
2012-10-17 17:41:11 +0800

    words module handling dictionaries
    Tested a bit and should work. Still some awkward overlap between scoring and words module.

--- utils/words.py
+++ utils/words.py
@@ -4,22 +4,14 @@
    more interesting.
 
 """
+import enchant, re, cPickle as pickle
 from nltk.corpus import wordnet
-import enchant, re
 en_check = enchant.Dict("en_GB").check
 
-#### todo #####
-##
-##  use words class in dict
-##
-
-class Word:
-    """
-    Word class
-    """
-    def __init__(self, rank, freq):
-        self.rank = rank
-        self.freq = freq
+def wordlist_filename():
+    return "data/en_filtered.txt"  
+def wordlist_dumpfilename():
+    return "data/reference_wordlist.dump"
 
 def filters():
     """
@@ -27,31 +19,58 @@ def filters():
     """
     return [en_check, wordnet.synsets]
 
+class Word:
+    """
+    Word class
+    """
+    def __init__(self, word, rank, freq):
+        self.word = word
+        self.rank = rank
+        self.freq = freq
 
 def is_english(word):
     return word.isalpha() and any([f(word) for f in filters()]) 
 
-def create_wordlist_csv(fname):
+def setup_reference_wordlist():
     """
-    Loads a list from file. Returns normalized {word:freq} dictionary.
+    Load reference wordlist from dump if available, create it from textfile
+    otherwise.
+    """
+    try:
+        d = pickle.load(open(wordlist_dumpfilename(), 'rb'))
+        print "Loaded previously generated reference wordlist."
+        return d
+    except:
+        print "Couldn't load reference wordlist."
+
+    print "Creating reference wordlist from textfile. May take a while."
+    d = create_wordlist_from_file(wordlist_filename())
+    print "Dumping reference wordlist to file for next time."
+    pickle.dump(d, open(wordlist_dumpfilename(), 'wb'), protocol=2)
+
+    return d
+
+def create_wordlist_from_file(fname):
+    """
+    Loads a list from file. Returns normalized {word:Word} dictionary.
 
     Column1: words
-    Column2: some frequency measure
+    Column2: some frequency measure in descending order
     """
-    f = open(fname, 'r')
-    d = {}
     total = 0.0
+    rank = 1
+    d = {}
 
-    for line in f.readlines():
-        word, freq = line.split()
-        if is_english(word):
-            d[word] = float(freq)
+    l = load_wordlist(fname)
+
+    for word, freq in l:
+            d[word] = Word(word, rank, float(freq))
+            rank += 1
             total += float(freq)
     
-    f.close()
-
+    # normalize
     for word in d:
-        d[word] /= total
+        d[word].freq /= total
 
     return d
 
@@ -74,11 +93,16 @@ def filter_wordlist(l):
 
 
 if __name__ == "__main__":
-    l = load_wordlist('data/en.txt')
+    fn = 'data/en.txt'
+    fnf = 'data/en_filtered.txt'
+    l = load_wordlist(fn)
     print "loaded"
     l = filter_wordlist(l)
     print "filtered"
-    save_wordlist('data/en_filtered.txt', l)
-
+    save_wordlist(fnf, l)
+    print "saved"
+    d = create_wordlist_from_file(fnf)
+    print "wordlist created"
+    print '"fuck" is the %i-th most used word' % d["fuck"].rank
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
